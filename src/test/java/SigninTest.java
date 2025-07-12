b5b3f6d6dc04f400af8a3c6717b865c47e47bd3f
husseinSabryCS <sabryhusseinhussein@gmail.com>
2025-07-12 12:53:49 +0300

    edit sign in class name

--- src/test/java/SigninTest.java
+++ src/test/java/SigninTest.java
@@ -1,12 +1,11 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.SignInPage;
 
-public class SigningTest {
+public class SigninTest {
     WebDriver driver;
     SignInPage signInPage;
     @BeforeMethod
